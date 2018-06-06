83d726355bb2772dbdfe247352a065fbb1d6cc36
Guilherme Lima <lima@fnal.gov>
2018-06-06 18:48:00 -0500

    Extended (vectorized) version of ApproxEqual() to handle vector-type arguments

--- vechep/base/ApproxEqual.hh
+++ vechep/base/ApproxEqual.hh
@@ -2,7 +2,8 @@
 //
 // @Purpose: Convenience functions for FP comparisons
 //
-// 20180601 Guilherme Lima -- Shamelessly copied from VecGeom unit tests
+// 20180601 Guilherme Lima -- Vectorized version, extended original from vecgeom/test/unit_test/ApproxEqual.h
+
 
 #ifndef APPROXEQUAL_HH
 #define APPROXEQUAL_HH
@@ -20,22 +21,34 @@ constexpr double kInfinity = vecCore::NumericLimits<double>::Max();
 // Return true is x if less than kApproxEqualTolerance from y
 
 template <typename T>
-bool ApproxEqual(T const& x, T const& y)
+vecCore::Mask_v<T> ApproxEqual(T const& x, T const& y)
 {
   using vecCore::math::Abs;
-  if (x == y) {
-    return true;
-  } else if (x * y == 0.0) {
-    T diff = Abs(x - y);
-    return diff < kApproxEqualTolerance;
-  } else if (Abs(x) > kInfinity || Abs(y) > kInfinity) {
-    // handle comparisons to infinity
-    return (x * y > 0) && Abs(x) > kInfinity && Abs(y) > kInfinity;
-  } else {
-    double diff  = Abs(x - y);
-    double abs_x = Abs(x), abs_y = Abs(y);
-    return diff / (abs_x + abs_y) < kApproxEqualTolerance;
+  vecCore::Mask_v<T> result( x == y );
+
+  if ( vecCore::VectorSize<T>() == 1 ) {
+    if ( vecCore::MaskFull( result ) ) return result;
   }
+
+  //  } else if (x * y == 0.0)
+  //    T diff = Abs(x - y);
+  //    return diff < kApproxEqualTolerance;
+  //  }
+  //result = result | 
+  //else if (Abs(x) > kInfinity || Abs(y) > kInfinity) {
+  //  // handle comparisons to infinity
+  //  return (x * y > 0) && Abs(x) > kInfinity && Abs(y) > kInfinity;
+  //} else {
+
+  T diff  = Abs(x - y);
+  //T abs_x = Abs(x), abs_y = Abs(y);
+  result = result | (diff < kApproxEqualTolerance);
+
+  //.. handle comparisons to infinity
+  //return (x * y > 0) && Abs(x) > kInfinity && Abs(y) > kInfinity;
+  result = result | ( (x * y > T(0.)) & (Abs(x) > kInfinity) & (Abs(y) > kInfinity) );
+
+  return result;
 }
 
 // Return true if the 3vector check is approximately equal to target
