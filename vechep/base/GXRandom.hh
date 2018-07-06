629338dbb360276004fbab58b201608913f32e9a
Geant4 Performance <g4p@tev.fnal.gov>
2018-07-06 15:35:44 -0500

    a minimal implementation for the random interface to VecRNG - WIP

---
+++ vechep/base/GXRandom.hh
@@ -0,0 +1,37 @@
+#ifndef GXRANDOM_H
+#define GXRANDOM_H
+
+#include "VecRng/MRG32k3a.h"
+#include "VecHepDefs.hh"
+
+namespace gxbert {
+inline namespace GXBERT_IMPL_NAMESPACE {
+
+class GXRandom 
+{
+public:
+  static vecRng::MRG32k3a<VectorBackend>& RNG();
+
+public:
+
+  VECCORE_ATT_HOST 
+  GXRandom() {}
+
+  VECCORE_ATT_HOST
+  ~GXRandom() {}
+
+  //add utilities
+  //redesign for multithreaded applications
+};
+
+vecRng::MRG32k3a<VectorBackend>& GXRandom::RNG(){
+  static vecRng::cxx::MRG32k3a<VectorBackend> theGenerator 
+    = vecRng::cxx::MRG32k3a<VectorBackend>();
+  theGenerator.Initialize();
+  return theGenerator;
+}
+
+} // end namespace impl
+} // end namespace gxbert
+
+#endif
